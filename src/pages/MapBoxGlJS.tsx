a00679334cbb306b7b0eca1d9c85b48967620f44
alvarezjo <jonathan.alvarez@axientcorp.com>
2024-11-01 09:38:04 -0500

    Project set up and imports

---
+++ src/pages/MapBoxGlJS.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MapBoxGlJS = () => {
+  return (
+    <div>MapBoxGlJS</div>
+  )
+}
+
+export default MapBoxGlJS
---
+++ src/pages/Nvidia.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Nvidia = () => {
+  return (
+    <div>Nvidia</div>
+  )
+}
+
+export default Nvidia
